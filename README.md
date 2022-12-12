054cb216eec7fb90c0a37a46f769505a350ca8f4
@aketchgithub <vickeryace@gmaill.com>
2022-12-12 17:48:27 +0300

    Complete

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # week-3-code-challenge
+
+ # Description
+This project implements a mini web app with utilization of array iteration, DOM, Events, and server communication.
+The application uses the route ("http://localhost:3000/films")
+
+
+# Getting Started
+In order for you to use the content on this repo ensure you have the following:
+
+- A computer that runs on either of the following; (Windows 7+, Linux, Mac OS)
+- nodejs 9.0+
+
+## Installation
+
+To use this repo on your machine requires some simple steps
+
+### Option One
+
+- Open a terminal / command line interface on your computer
+- Clone the repo by using the following:
+
+        git clone "git+https://github.com/aketchgithub/week-3-code-challenge.git"
+
+- Change directory to the repo folder:
+
+        cd week-3-code-challenge
+
+- (Optional) Open it in ``Visual Studio Code``
+
+        code .
+
+- (Alternate Option) Open it in any editor of your choice.
+
+### Alternative Two
+
+- On the top right corner of this page there is a button labelled ``Fork``.
+- Click on that button to fork the repo to your own account.
+- Take on the process in ``Alternative One`` above.
+- Remember to replace your username when cloning.
+
+        git clone https://github.com/your-username-here/week-3-code-challenge.git
+
+
+
+# Running the application
+
+- Install required dependencies from npm
+
+      npm install
+
+- Run the application on terminal
+
+      npm serve
+
+- Run on the browser
+
+      npm run start
+
+# Dependencies
+
+"dependencies": {
+    "json-server": "^0.17.1"
+
+# Authors
+This project was contributed to by:
+- [Victor Aketch](https://github.com/aketchgithub)
+
+# License
+The project is licensed under Apache 2.0.
---
+++ js/main.js
@@ -0,0 +1,165 @@
+const url = "http://localhost:3000/films"
+const firstFilmApi = url
+
+document.addEventListener('DOMContentLoaded', () => {
+   const fetchFirstFilm = () => {
+      fetch(url)
+      .then(resp => resp.json())
+      .then(film =>renderFirstFilm(film[0])
+      )
+
+       //console.log(data)
+   }
+    fetchFirstFilm();
+})
+
+function renderFirstFilm(film) {
+
+ let button = document.getElementById('button')
+ button.innerHTML=''
+
+ const poster = document.getElementById('poster')
+ poster.src = film.poster
+
+ const title = document.getElementById('title')
+ title.innerText = film.title
+
+ const runtime = document.getElementById('runtime')
+ runtime.innerText = `${film.runtime} MINUTES`
+
+ const showtime = document.getElementById('showtime')
+ showtime.innerText = film.showtime
+
+ const description = document.getElementById('film-info')
+ description.innerText = film.description
+
+ const tickets= document.getElementById('ticket-num')
+ tickets.innerText = film.capacity-film.tickets_sold
+
+ let btn = document.createElement('button')
+ let ticketsAvailable= film.capacity-film.tickets_sold
+ btn.addEventListener('click',() => {
+   if(ticketsAvailable > 0){
+      ticketsAvailable -=1
+      document.getElementById('ticket-num').innerText=ticketsAvailable
+      
+     }
+     else if( ticketsAvailable<1){
+      document.getElementById('ticket-num').innerText='Sold Out!'
+     }
+ } )
+ btn.innerText= "Buy Tickets"
+ button.appendChild(btn)
+
+ 
+
+}
+function menu() {
+   // document.getElementById(menu)
+   // let filmMenu = document.createElement('ul') 
+   // filmMenu.classList.add('menu')
+   //       filmMenu.innerText= 'menu'
+   //      menu.innerText = film.menu
+      fetch(url)
+         .then(resp => resp.json())
+         .then(data =>{
+            data.forEach(item => {
+
+      let titles = document.createElement("li")
+      titles.addEventListener('click', ()=> {
+         console.log("clicked")
+         const i = item.id
+         renderPoster(data[i-1])
+
+      })
+      titles.innerText= item.title
+      let menuList = document.getElementById('titleList')
+      menuList.appendChild(titles)
+            })
+         })
+} 
+ menu()
+//  const ticketSpan = document.getElementById('ticket-num')
+
+//  const capacity = parseInt(film.capacity)
+//  ticketSpan.innerText = capacity - film.tickets_sold
+
+
+//  const button = document.querySelector('ui.green.button')
+//  button.addEventListener("click", (e) => {
+//    if (ticketSpan.innerText <= 0) {
+//       let buttonDiv=
+//       document.querySelector('.extra.content');
+//       buttonDiv.innerHTML = "<button>Sold Out!</button>" 
+//       document.append
+//    }
+//    else {
+//       const tickets_sold = film.tickets_sold + 1
+//       let obj = {tickets_sold}
+
+//       updateTicketNum(obj)
+//    }
+//  })
+ 
+
+function renderPoster(film){
+
+ let button = document.getElementById('button')
+ button.innerHTML=''
+
+ const poster = document.getElementById('poster')
+ poster.src = film.poster
+
+ const title = document.getElementById('title')
+ title.innerText = film.title
+
+ const runtime = document.getElementById('runtime')
+ runtime.innerText = `${film.runtime} MINUTES`
+
+ const showtime = document.getElementById('showtime')
+ showtime.innerText = film.showtime
+
+ const description = document.getElementById('film-info')
+ description.innerText = film.description
+
+ const tickets= document.getElementById('ticket-num')
+ tickets.innerText = film.capacity-film.tickets_sold
+let ticketsAvailable= film.capacity-film.tickets_sold
+ let btn = document.createElement('button')
+ btn.addEventListener('click',() => {
+     //console.log('I was clicked')
+     if(ticketsAvailable > 0){
+      ticketsAvailable -=1
+      document.getElementById('ticket-num').innerText=ticketsAvailable
+      
+     }
+     else if( ticketsAvailable<1){
+      document.getElementById('ticket-num').innerText='Sold Out!'
+     }
+ })
+ btn.innerText= "Buy Tickets"
+ button.appendChild(btn)
+
+
+ }
+
+ function buyTickets(){
+   //console.log('clicked')
+
+ }
+
+ function updateTicketNum(obj) {
+   options = {
+      method: 'PATCH',
+      headers: {
+         "content-type": "application/json",
+         "accept": "application/json"
+      },
+      body: JSON.stringify(obj)
+   }
+   fetch((url + id), options)
+      .then(response => response.json())
+      .then(data => {
+         renderFirstFilm(data)
+      })
+}
